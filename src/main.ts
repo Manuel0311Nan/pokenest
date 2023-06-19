056d53089e4dc446e488c23f135686ccd97427ae
Manuel Alcázar <manuzz3112@gmail.com>
2023-06-19 06:56:27 +0200

    Fisrt Commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  //InfoReadme: Se establece un prefijo a la hora de establecer todos los controladores
+  app.setGlobalPrefix('api/v2');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted:true,
+    })
+  )
+  await app.listen(3000);
+}
+bootstrap();
